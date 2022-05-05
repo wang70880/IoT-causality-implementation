3e2460c2c3deeca65b192ff24b87acafd2924b5c
wang70880 <wjcuhk@gmail.com>
2022-05-05 10:43:40 +0800

    [Testing] Set selected_links to be the golden standard and find appropriateparameters (pc_alpha, alpha_level) to maximize the accuracy.

---
+++ evaluations/discovery-accuracy/paint-histogram.py
@@ -0,0 +1,9 @@
+import numpy as np 
+from matplotlib import pyplot as plt
+
+def draw_pcmci_accuracy():
+    """ The x-axis should be the optional parameter: pc_alpha and alpha_level
+        For each x_axis, there are two columns, each of which represents precision or recall.z
+        The y-axis should be the ratio %.
+    """
+    pass
--- src/background_generator.py
+++ src/background_generator.py
@@ -59,7 +59,7 @@ class BackgroundGenerator():
                 heuristic_attr_array = heuristic_temporal_pair_dict[frame_id][lag]
                 for idx, x in np.ndenumerate(attr_array):
                     """JC TODO: Explain here why we set 5 as the golden standard and why our heuristic standard is set to 4."""
-                    heuristic_attr_array[idx] = 0 if x < 2 * self.partition_config[1] else 1
+                    heuristic_attr_array[idx] = 0 if x < 3 * self.partition_config[1] else 1
                     attr_array[idx] = 0 if x < 4 * self.partition_config[1] else 1  
                 print("sum(heuristic_temporal_array) - sum(temporal_array) for frame_id = {}, lag = {} is {}".format(frame_id, lag, np.sum(heuristic_attr_array) - np.sum(attr_array)))
 
