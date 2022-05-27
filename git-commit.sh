a14a4ccfb0513f866f08816647086b3622ad77fa
wang70880 <wjcuhk@gmail.com>
2022-05-27 11:29:20 +0800

    [Debugging] Find out why the detection accuracy of type1 anomalies is low.

--- git-commit.sh
+++ git-commit.sh
@@ -2,6 +2,6 @@
 com="[Debugging] Find out why the detection accuracy of type1 anomalies is low."
 git add -A
 git commit -m "$com"
-git push
+git push --set-upstream origin breakpoint-theory
 
 cd src/tigramite; git add -A; git commit -m "$com"; git push
