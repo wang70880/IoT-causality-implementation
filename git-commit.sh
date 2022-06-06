155d5140424425981cca271116367f29d356f546
wang70880 <wjcuhk@gmail.com>
2022-06-06 13:23:05 +0800

    [Debugging] Find out the reason why the accuracy of score estimation is so low.

--- git-commit.sh
+++ git-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-com="[Implementation] Implement the anomaly score threshold calculation and the type 2 anomaly detection. [Testing] Test the accuracy of the threshold calculation function, and the runtime type 2 anomaly detection."
+com="[Debugging] Find out the reason why the accuracy of score estimation is so low."
 git add -A
 git commit -m "$com"
 git push --set-upstream origin breakpoint-theory
