dfd00c40565e98f6c3a9715fade463091720fed6
wang70880 <wjcuhk@gmail.com>
2022-06-17 14:15:54 +0800

    [Debugging] Find out the reason why the number of false positives of score estimation is high.

--- causalIoT.py
+++ causalIoT.py
@@ -247,6 +247,8 @@ if TEST_PARAM_SETTING:
     single_frame_test_flag = 1 # JC TEST: Test for single dataframe
     skip_skeleton_estimation_flag = 0 # JC TEST: Skip the causal discovery algorithm
     skip_bayesian_fitting_flag = 0
+    num_anomalies = 0
+    max_prop_length = 1
 
 if PARAM_SETTING:
     dataset = 'hh101'
@@ -395,7 +397,7 @@ for frame_id in range(event_preprocessor.frame_count):
         print("[Security guarding] Testing log starting positions {} with score threshold {}.".format(frame['testing-start-index'] + 1, security_guard.score_threshold))
         # 1. Inject device anomalies
         #testing_event_sequences = list(zip(frame['testing-attr-sequence'], frame['testing-state-sequence'])); true_anomaly_positions = []
-        testing_event_sequences, anomaly_starting_positions, benign_position_dict = evaluator.inject_anomalies(frame_id=frame_id, n_anomalies=0, maximum_length=1)
+        testing_event_sequences, anomaly_starting_positions, benign_position_dict = evaluator.inject_anomalies(frame_id=frame_id, n_anomalies= num_anomalies, maximum_length= max_prop_length)
         # 2. Initiate anomaly detection
         start = time.time()
         event_id = 0
