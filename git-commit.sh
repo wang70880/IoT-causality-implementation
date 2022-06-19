08e40d6a030745b29d7da506e2684241401432ba
wang70880 <wjcuhk@gmail.com>
2022-06-19 15:53:58 +0800

    [Debugging] Reason for the high false positive of devices M001.

--- git-commit.sh
+++ git-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-com="[Implementation] Do not compute anomaly scores (as weill as inject anomalies) for attributes which have no interactions (i.e., have 0 incomming degrees or only have automacorrelations)."
+com="[Debugging] Reason for the high false positive of devices M001."
 git add -A
 git commit -m "$com"
 git push --set-upstream origin breakpoint-theory
