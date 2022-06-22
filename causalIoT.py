ebeaf3fbc060759f44e209ca6f58636d34b55b90
wang70880 <wjcuhk@gmail.com>
2022-06-22 15:27:23 +0800

    [Implementation] Implement the anomaly injection scheme: Case of malicious control; [Debugging] Find out why the accuracy is low: Print out the CPT of device D002, which is a good debugging point.

--- causalIoT.py
+++ causalIoT.py
@@ -221,7 +221,10 @@ class BayesianFitter:
         #cpd = MaximumLikelihoodEstimator(self.model, df).estimate_cpd(corrs_attr) # JC TEST: The bayesian fitting consumes much time. Let's test the exact consumed time here..
         #print(cpd)
         self.model.fit(df, estimator= BayesianEstimator) 
-        print(self.model.get_cpds('D002'))
+        D002_model = self.model.get_cpds('D002').copy()
+        print(D002_model)
+        D002_model.marginalize(['D002(-1)'])
+        print(D002_model)
     
     def predict_attr_state(self, attr, parent_state_dict, verbose=0):
         """ Predict the value of the target attribute given its parent states, i.e., E[attr|par(attr)]
