5f97f1be32d225647acb7bfdf7806bddbe4b365a
wang70880 <wjcuhk@gmail.com>
2022-07-29 10:16:04 +0800

    [Breakpoint] Prepare to update the shell script and the parameter setting: For evaluation purpose.

--- git-commit.sh
+++ git-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-com="[Implementation] Update Bayesian Fitter module."
+com="[Breakpoint] Prepare to update the shell script and the parameter setting: For evaluation purpose."
 git add -A
 git commit -m "$com"
 git push --set-upstream origin breakpoint-theory
