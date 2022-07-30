9260a8da8dc89681dc1b807709ee726d58d96d0d
wang70880 <wjcuhk@gmail.com>
2022-07-30 11:12:54 +0800

    [Implementation] Finish the implementation of the data preprocessing module.

--- git-commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-com="[Implementation] Finish the implementation of the data preprocessing module."
-git add -A
-git commit -m "$com"
-git push --set-upstream origin breakpoint-theory
-
-cd src/tigramite; git add -A; git commit -m "$com"; git push
