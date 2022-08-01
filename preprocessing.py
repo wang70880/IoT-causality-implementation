57cdc70d6c4d7e899ff06d634a0851c24c483521
wang70880 <wjcuhk@gmail.com>
2022-08-01 16:17:55 +0800

    [Implementation] Implement the g-sqaure test. Prepare to test it.

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,12 @@
 import os, sys
 from src.event_processing import Hprocessor
+from time import time
 
+def _elapsed_minutes(start):
+    return (time()-start) * 1.0 / 60
+
+dp_start = time()
 dataset = sys.argv[1]; verbosity = int(sys.argv[2])
 event_preprocessor:'Hprocessor' = Hprocessor(dataset, verbosity)
 event_preprocessor.initiate_data_preprocessing()
+print("[Data sanitization] Complete ({} minutes)".format(_elapsed_minutes(dp_start)))
